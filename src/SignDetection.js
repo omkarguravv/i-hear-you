cb3dfdfdf1d7a28d0fdf00865ad21fe7194becc7
Bhushan <bhushanpawar9001@gmail.com>
2024-01-31 22:59:04 +0530

    sign Detetction on Webcam

--- src/SignDetection.js
+++ src/SignDetection.js
@@ -1,31 +1,153 @@
+// // Import dependencies
+// import React, { useRef, useState, useEffect } from "react";
+// import * as tf from "@tensorflow/tfjs";
+// import * as cocossd from "@tensorflow-models/coco-ssd";
+
+// // 1. TODO - Import required model here
+// // e.g. import * as tfmodel from "@tensorflow-models/tfmodel";
+// import Webcam from "react-webcam";
+// import "./App.css";
+// import { drawrect } from "./utilities";
+// // 2. TODO - Import drawing utility here
+// // e.g. import { drawRect } from "./utilities";
+
+// function SignDetection() {
+//   const [objects, setObject] = useState([]);
+//   const webcamRef = useRef(null);
+//   const canvasRef = useRef(null);
+
+//   // Main function
+//   const runCoco = async () => {
+//     // 3. TODO - Load network
+//     // e.g. const net = await cocossd.load();
+//     const net = await cocossd.load();
+
+//     //  Loop and detect hands
+//     setInterval(() => {
+//       detect(net);
+//     }, 10);
+//   };
+
+//   const detect = async (net) => {
+//     // Check data is available
+//     if (
+//       typeof webcamRef.current !== "undefined" &&
+//       webcamRef.current !== null &&
+//       webcamRef.current.video.readyState === 4
+//     ) {
+//       // Get Video Properties
+//       const video = webcamRef.current.video;
+//       const videoWidth = webcamRef.current.video.videoWidth;
+//       const videoHeight = webcamRef.current.video.videoHeight;
+
+//       // Set video width
+//       webcamRef.current.video.width = videoWidth;
+//       webcamRef.current.video.height = videoHeight;
+
+//       // Set canvas height and width
+//       canvasRef.current.width = videoWidth;
+//       canvasRef.current.height = videoHeight;
+
+//       // 4. TODO - Make Detections
+//       // e.g. const obj = await net.detect(video);
+//       const obj = await net.detect(video);
+//       //   console.log(obj);
+//       setObject(obj);
+
+//       // Draw mesh
+//       const ctx = canvasRef.current.getContext("2d");
+
+//       // 5. TODO - Update drawing utility
+//       // drawSomething(obj, ctx)
+//       drawrect(obj, ctx);
+//     }
+//   };
+
+//   useEffect(() => {
+//     runCoco();
+//   }, []);
+
+//   return (
+//     <>
+//       <div className=" flex flex-col md:flex-row  min-h-screen w-full jusitify-center">
+//         <header className="w-full md:w-1/2">
+//           <Webcam
+//           // className=" left-0 right-0 mx-auto absolute z-9 text-center w-[640px] h-[480px]"
+//             ref={webcamRef}
+//             muted={true}
+//             // style={{
+//             //   position: "absolute",
+//             //   marginLeft: "auto",
+//             //   marginRight: "auto",
+//             //   left: 0,
+//             //   right: 0,
+//             //   textAlign: "center",
+//             //   zindex: 9,
+//             //   width: 640,
+//             //   height: 480,
+//             // }}
+//           />
+
+//           <canvas
+//           className="hidden left-0 right-0  mx-auto absolute z-8 text-center w-[640px] h-[480px]"
+//             ref={canvasRef}
+//             // style={{
+//             //   position: "absolute",
+//             //   marginLeft: "auto",
+//             //   marginRight: "auto",
+//             //   left: 0,
+//             //   right: 0,
+//             //   textAlign: "center",
+//             //   zindex: 8,
+//             //   width: 640,
+//             //   height: 480,
+//             // }}
+//           />
+//         </header>
+//         <div className="w-full md:w-1/2 text-center static">
+//           {objects.map((object, index) => (
+//             <div key={index}>
+//               <p className="text-2xl font-bold md:text-7xl">{object.class}</p>
+//               {/* <p>Bbox: x={object.bbox[0]}, y={object.bbox[1]}, width={object.bbox[2]}, height={object.bbox[3]}</p> */}
+//               {/* <p>Confidence Score: {object.score}</p> */}
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default SignDetection;
+
 // Import dependencies
 import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
-import * as cocossd from "@tensorflow-models/coco-ssd";
-
-// 1. TODO - Import required model here
-// e.g. import * as tfmodel from "@tensorflow-models/tfmodel";
 import Webcam from "react-webcam";
 import "./App.css";
-import { drawrect } from "./utilities";
+// import model from "./model.json"
+import { nextFrame } from "@tensorflow/tfjs";
 // 2. TODO - Import drawing utility here
 // e.g. import { drawRect } from "./utilities";
+import {drawRect} from "./utilities"; 
 
-function SignDetection() {
-  const [objects, setObject] = useState([]);
+function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
   // Main function
   const runCoco = async () => {
-    // 3. TODO - Load network
+    // 3. TODO - Load network 
     // e.g. const net = await cocossd.load();
-    const net = await cocossd.load();
+    // https://tensorflowjsrealtimemodel.s3.au-syd.cloud-object-storage.appdomain.cloud/model.json
+    //https://raw.githubusercontent.com/omkarguravv/i-hear-you/main/model/test.json
+    //https://raw.githubusercontent.com/omkarguravv/i-hear-you/main/model/model.json
+    const net = await tf.loadGraphModel('https://raw.githubusercontent.com/omkarguravv/i-hear-you/main/model/model.json')
 
     //  Loop and detect hands
     setInterval(() => {
       detect(net);
-    }, 10);
+    }, 16.7);
   };
 
   const detect = async (net) => {
@@ -49,73 +171,72 @@ function SignDetection() {
       canvasRef.current.height = videoHeight;
 
       // 4. TODO - Make Detections
-      // e.g. const obj = await net.detect(video);
-      const obj = await net.detect(video);
-      //   console.log(obj);
-      setObject(obj);
+      const img = tf.browser.fromPixels(video)
+      const resized = tf.image.resizeBilinear(img, [640,480])
+      const casted = resized.cast('int32')
+      const expanded = casted.expandDims(0)
+      const obj = await net.executeAsync(expanded)
+      console.log(obj)
 
+      const boxes = await obj[6].array()
+      const classes = await obj[5].array()
+      const scores = await obj[1].array()
+      
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
 
       // 5. TODO - Update drawing utility
-      // drawSomething(obj, ctx)
-      drawrect(obj, ctx);
+      // drawSomething(obj, ctx)  
+      requestAnimationFrame(()=>{drawRect(boxes[0], classes[0], scores[0], 0.8, videoWidth, videoHeight, ctx)}); 
+
+      tf.dispose(img)
+      tf.dispose(resized)
+      tf.dispose(casted)
+      tf.dispose(expanded)
+      tf.dispose(obj)
+
     }
   };
 
-  useEffect(() => {
-    runCoco();
-  }, []);
+  useEffect(()=>{runCoco()},[]);
 
   return (
-    <>
-      <div className=" flex flex-col md:flex-row  min-h-screen w-full jusitify-center">
-        <header className="w-full md:w-1/2">
-          <Webcam
-          // className=" left-0 right-0 mx-auto absolute z-9 text-center w-[640px] h-[480px]"
-            ref={webcamRef}
-            muted={true}
-            // style={{
-            //   position: "absolute",
-            //   marginLeft: "auto",
-            //   marginRight: "auto",
-            //   left: 0,
-            //   right: 0,
-            //   textAlign: "center",
-            //   zindex: 9,
-            //   width: 640,
-            //   height: 480,
-            // }}
-          />
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          muted={true} 
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
 
-          <canvas
-          className="hidden left-0 right-0  mx-auto absolute z-8 text-center w-[640px] h-[480px]"
-            ref={canvasRef}
-            // style={{
-            //   position: "absolute",
-            //   marginLeft: "auto",
-            //   marginRight: "auto",
-            //   left: 0,
-            //   right: 0,
-            //   textAlign: "center",
-            //   zindex: 8,
-            //   width: 640,
-            //   height: 480,
-            // }}
-          />
-        </header>
-        <div className="w-full md:w-1/2 text-center static">
-          {objects.map((object, index) => (
-            <div key={index}>
-              <p className="text-2xl font-bold md:text-7xl">{object.class}</p>
-              {/* <p>Bbox: x={object.bbox[0]}, y={object.bbox[1]}, width={object.bbox[2]}, height={object.bbox[3]}</p> */}
-              {/* <p>Confidence Score: {object.score}</p> */}
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+    </div>
   );
 }
 
-export default SignDetection;
+export default App;
+
--- src/utilities.js
+++ src/utilities.js
@@ -1,17 +1,47 @@
-export const drawrect = (detections,ctx)=>{
-    detections.forEach(prediction => {
-        const [x,y,width,height] = prediction['bbox'];
-        const text = prediction['class'];
+// export const drawrect = (detections,ctx)=>{
+//     detections.forEach(prediction => {
+//         const [x,y,width,height] = prediction['bbox'];
+//         const text = prediction['class'];
         
-        const color = 'green';
-        ctx.strokeStyle = color;
-        ctx.font = '18px Cursive';
-        ctx.fillStyle = 'black';
+//         const color = 'green';
+//         ctx.strokeStyle = color;
+//         ctx.font = '18px Cursive';
+//         ctx.fillStyle = 'black';
         
-        ctx.beginPath();
-        ctx.fillText(text,x,y);
-        ctx.rect(x,y,width,height);
-        ctx.stroke();
+//         ctx.beginPath();
+//         ctx.fillText(text,x,y);
+//         ctx.rect(x,y,width,height);
+//         ctx.stroke();
 
-    });
+//     });
+// }
+const labelMap = {
+    1:{name:'hello', color:'red'},
+    2:{name:'namaste', color:'yellow'},
+    // 3:{name:'I Love You', color:'lime'},
+    // 4:{name:'Yes', color:'blue'},
+    // 5:{name:'No', color:'purple'},
+}
+
+// Define a drawing function
+export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight, ctx)=>{
+    for(let i=0; i<=boxes.length; i++){
+        if(boxes[i] && classes[i] && scores[i]>threshold){
+            // Extract variables
+            const [y,x,height,width] = boxes[i]
+            const text = classes[i]
+            
+            // Set styling
+            ctx.strokeStyle = labelMap[text]['color']
+            ctx.lineWidth = 10
+            ctx.fillStyle = 'black'
+            ctx.font = '30px Arial'         
+            
+            // DRAW!!
+            ctx.beginPath()
+            ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i]*100), x*imgWidth, y*imgHeight-10)
+            ctx.rect(x*imgWidth, y*imgHeight, width*imgWidth/2, height*imgHeight/1.5);
+            ctx.stroke()
+        }
+    }
 }
