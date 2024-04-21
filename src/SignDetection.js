283724044e9c3f498e2f0b679a3f54c61ba56e67
Bhushan <bhushanpawar9001@gmail.com>
2024-04-21 12:46:46 +0530

    new model added

--- src/SignDetection.js
+++ src/SignDetection.js
@@ -9,7 +9,7 @@ import { nextFrame } from "@tensorflow/tfjs";
 // e.g. import { drawRect } from "./utilities";
 import { drawRect } from "./utilities";
 
-const COCO_CLASSES = ["namaste", "hello"];
+
 const videoConstraintsEnvironment = {
   // width: 1280,
   // height: 720,
@@ -21,7 +21,7 @@ const videoConstraintsUser = {
   facingMode: "user",
 };
 function App() {
-  const [detectedObject, setDetectedObject] = useState("");
+  // const [detectedObject, setDetectedObject] = useState("");
 
   const [activeCamera, setActiveCamera] = useState(videoConstraintsUser);
 
@@ -84,8 +84,7 @@ function App() {
       const classes = await obj[5].array();
       const scores = await obj[1].array();
 
-      const detectedObjectName = COCO_CLASSES[classes[0][0]] || "Unknown";
-      setDetectedObject(detectedObjectName);
+      
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
 
@@ -125,17 +124,17 @@ function App() {
             muted={true}
             // facingMode={activeCamera}
             videoConstraints={activeCamera}
-            // style={{
-            //   position: "absolute",
-            //   marginLeft: "auto",
-            //   marginRight: "auto",
-            //   left: 0,
-            //   right: 0,
-            //   textAlign: "center",
-            //   zindex: 9,
-            //   width: 640,
-            //   height: 480,
-            // }}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: 9,
+              width: 640,
+              height: 480,
+            }}
           />
           <button
             className="mt-10 inline-flex items-center gap-1 group/button rounded-full hover:scale-105 focus:outline-none transition ring-offset-gray-900 bg-black shadow-xl shadow-black/20 ring-1 [&:not(:focus)]:ring-inset ring-gray-700/30 hover:bg-gray-700/70 focus:ring-gray-600 focus:ring-offset-2 text-base font-medium px-5 py-2.5 justify-center text-white"
@@ -144,24 +143,22 @@ function App() {
             Toggle Camera
           </button>
           <canvas
-            className="hidden left-0 right-0  mx-auto absolute z-8 text-center w-[640px] h-[480px]"
+            // className="hidden left-0 right-0  mx-auto absolute z-8 text-center w-[640px] h-[480px]"
             ref={canvasRef}
-            // style={{
-            //   position: "absolute",
-            //   marginLeft: "auto",
-            //   marginRight: "auto",
-            //   left: 0,
-            //   right: 0,
-            //   textAlign: "center",
-            //   zindex: 8,
-            //   width: 640,
-            //   height: 480,
-            // }}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: 8,
+              width: 640,
+              height: 480,
+            }}
           />
         </header>
-        <div className="w-full md:w-1/2 text-center static">
-          <p className="text-2xl font-bold md:text-7xl">{detectedObject}</p>
-        </div>
+        
       </div>
     </>
   );
